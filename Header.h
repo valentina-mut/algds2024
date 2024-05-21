1146634c0aa275b1bc39af9946729e1d9afb52b4
valentina-mut <159520924+valentina-mut@users.noreply.github.com>
2024-05-21 13:39:14 +0300

    Муталимова 5030102/20001 лаб2 вар1

--- Header.h
+++ Header.h
@@ -1,16 +1,27 @@
-#pragma once
 
+#pragma once
 #include <stdio.h>
 #include <stdlib.h>
-
-struct TreeNode {
-	struct TreeNode* left;
-	struct TreeNode* right;
+#include "Header.h"
+struct Node {
+	struct Node* left;
+	struct Node* right;
 	int data;
 	int height;
 };
+struct Trunk
+{
+	Trunk* prev;
+	std::string str;
 
-typedef struct TreeNode Node;
+	Trunk(Trunk* prev, std::string str)
+	{
+		this->prev = prev;
+		this->str = str;
+	}
+};
+typedef struct Node Node;
 
-Node* insert(Node* root, int val);
-Node* erase(Node* root, int val);
+Node* Add(Node* root, int data);
+Node* Delete(Node* root, int deldata);
+void printTree(Node* root, Trunk* prev, bool isLeft);
